14b39cf4c70074ef201e35b56c83bcc02464f431
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-10-16 17:07:28 +0200

    migrate to atomic bundles

---
+++ views/build/grunt/bundle.js
@@ -0,0 +1,44 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2018 (original work) Open Assessment Technologies SA;
+ */
+
+/**
+ * configure the extension bundles
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.config.merge({
+        bundle : {
+            xmledit : {
+                options : {
+                    extension : 'xmlEdit',
+                    outputDir : 'loader',
+                    bundles : [{
+                        name : 'xmlEdit',
+                        default : true
+                    }]
+                }
+            }
+        }
+    });
+
+    // bundle task
+    grunt.registerTask('xmleditbundle', ['bundle:xmledit']);
+
+};
