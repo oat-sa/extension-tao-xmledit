d28308bc94b083367bb3f0ba28b53c43848dcae9
zagovorychev <zagovorichev@gmail.com>
2019-03-07 11:02:39 +0300

    Deleted redundant construct

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -55,6 +55,6 @@ class Updater extends common_ext_ExtensionUpdater
 
         $this->setVersion($currentVersion);
 
-        $this->skip('0.2.0', '3.0.0');
+        $this->skip('0.2.0', '3.0.1');
     }
 }
